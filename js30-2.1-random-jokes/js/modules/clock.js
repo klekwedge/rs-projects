e26aed4fe732c39a153a49eeecdfd8223a95d155
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-21 10:37:00 +0300

    fix: change the clock dial

--- js30-2.1-random-jokes/js/modules/clock.js
+++ js30-2.1-random-jokes/js/modules/clock.js
@@ -1,17 +1,12 @@
-'use strict';
-
+"use strict";
 function clock() {
-  const timeHours = document.querySelector(".date__hours div");
-  const timeMinutes = document.querySelector(".date__minutes div");
-  const timeSeconds = document.querySelector(".date__seconds div ");
-
   function getTime() {
     const date = new Date();
     return date;
   }
 
   function dateColon(date) {
-    if(date < 10){
+    if (date < 10) {
       return `0${date}`;
     }
     return date;
@@ -19,10 +14,34 @@ function clock() {
 
   setInterval(() => {
     const newTime = getTime();
-    timeHours.textContent = dateColon(newTime.getHours());
-    timeMinutes.textContent = dateColon(newTime.getMinutes());
-    timeSeconds.textContent = dateColon(newTime.getSeconds());
-  
+
+    const hours = document.querySelector("#hours");
+    const minutes = document.querySelector("#minutes");
+    const seconds = document.querySelector("#seconds");
+
+    const hourStep = document.querySelector("#hh");
+    const minuteStep = document.querySelector("#mm");
+    const secondStep = document.querySelector("#ss");
+
+    const hourDot = document.querySelector(".hour_dot");
+    const minDot = document.querySelector(".min_dot");
+    const secDot = document.querySelector(".sec_dot");
+
+    const h =  dateColon(newTime.getHours());
+    const m = dateColon(newTime.getMinutes());
+    const s = dateColon(newTime.getSeconds());
+
+    hours.innerHTML = h + "<br><span>Часы</span>";
+    minutes.innerHTML = m + "<br><span>Минуты</span>";
+    seconds.innerHTML = s + "<br><span>Секунды</span>";
+
+    hourStep.style.strokeDashoffset = 440 - (440 * h) / 24;
+    minuteStep.style.strokeDashoffset = 440 - (440 * m) / 60;
+    secondStep.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+    hourDot.style.transform = `rotate(${h * 15}deg)`;
+    minDot.style.transform = `rotate(${m * 6}deg)`;
+    secDot.style.transform = `rotate(${s * 6}deg)`;
   }, 1000);
 }
 
