dfee1d665c0a1ca3184a4468cf7c895758fe4a52
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-29 22:07:02 +0300

    feat: add a separate template for the movie modal

--- js30-2.3-movie-app/js/script.js
+++ js30-2.3-movie-app/js/script.js
@@ -11,9 +11,6 @@ const resetButton = document.querySelector(".header__reset");
 const searchButton = document.querySelector(".header__search");
 const homepageButton = document.querySelector(".header__image");
 
-
-
-
 let url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1`;
 getData();
 /*-------------------------------------------------------------*/
@@ -33,50 +30,32 @@ async function getData() {
 
 const showData = function (data) {
   for (let i = 0; i < data.results.length; i++) {
-    newElement(data.results[i]);
+    createMovieCard(data.results[i]);
   }
 };
 
 const templateMovie = document.querySelector("#card__movie").content;
 
-const newElement = function (newMovie) {
+const createMovieCard = function (newMovie) {
   const tempInstance = templateMovie.cloneNode(true);
   const movieItem = tempInstance.querySelector(".movie");
 
   const imageMovie = tempInstance.querySelector(".movie .movie__image");
-  const imageInfoMovie = tempInstance.querySelector(
-    ".movie__detail .movie__image"
-  );
+
   if (newMovie.poster_path !== null) {
     imageMovie.src = "https://image.tmdb.org/t/p/w1280" + newMovie.poster_path;
     imageMovie.alt = `${newMovie.original_title} image`;
-
-    imageInfoMovie.src = imageMovie.src;
-    imageInfoMovie.alt = imageMovie.alt;
   } else {
-    imageMovie.src = "assets/photo/Noimage.svg.png";
-    imageInfoMovie.src = imageMovie.src;
+    imageMovie.src = "assets/img/Noimage.svg.png";
   }
 
-  const titleMovie = tempInstance.querySelector(
-    ".movie__content .movie__title"
-  );
+  const titleMovie = tempInstance.querySelector(".movie__title");
   titleMovie.textContent = newMovie.title;
 
-  const titleInfoMovie = tempInstance.querySelector(
-    ".movie__info .info__title"
-  );
-  titleInfoMovie.textContent = newMovie.title;
 
-  const ratingMovie = tempInstance.querySelector(
-    ".movie__content  .movie__rating"
-  );
-  const ratingInfoMovie = tempInstance.querySelector(
-    ".movie__info  .info__rating"
-  );
+  const ratingMovie = tempInstance.querySelector(".movie__rating");
 
   ratingMovie.textContent = newMovie.vote_average;
-  ratingInfoMovie.textContent = "Vote average: " + ratingMovie.textContent;
 
   if (newMovie.vote_average >= 7) {
     ratingMovie.style.color = "#3BB33B";
@@ -86,39 +65,78 @@ const newElement = function (newMovie) {
     ratingMovie.style.color = "#FB0007";
   }
 
-  const ratingCountMovie = tempInstance.querySelector(".info__rating-count");
-  ratingCountMovie.textContent = "Rating count: " + newMovie.vote_count;
-
-  const popularityMovie = tempInstance.querySelector(".info__popularity");
-  popularityMovie.textContent = "Popularity: " + newMovie.popularity;
-
-  const releaseMovie = tempInstance.querySelector(".info__release");
-  releaseMovie.textContent = "Release: " + newMovie.release_date;
-
-  const languageMovie = tempInstance.querySelector(".info__language");
-  languageMovie.textContent =
-    "Original language: " + newMovie.original_language;
-
-  const overviewMovie = tempInstance.querySelector(".info__overview");
-  overviewMovie.textContent = newMovie.overview;
-
-  const infoMovie = tempInstance.querySelector(".movie__detail");
-  const mainmovie = tempInstance.querySelector(".movie__main");
-  //const substrateStructure= tempInstance.querySelector(".movie__substrate");
-  
   movieList.appendChild(movieItem);
 
-  mainmovie.addEventListener("click", function () {
-    infoMovie.style.top = `${movieItem.offsetTop}px`;
-    infoMovie.style.display = "flex";
+  createMovieModal(newMovie);
+
+  movieItem.addEventListener("click", function () {
+    
   });
 
-  infoMovie.addEventListener("click", function () {
-    infoMovie.style.display = "none";
-  });
 
 };
 
+function createMovieModal(newMovie) {
+  const templateModalMovie = document.querySelector("#modal__movie").content;
+  const movieModalTemplate = templateModalMovie.cloneNode(true);
+  const movieItem = movieModalTemplate.querySelector(".movie__overlay");
+  movieItem.classList.add('movie__detail');
+
+
+  const imageMovie = movieModalTemplate.querySelector(".movie__image");
+
+  if (newMovie.poster_path !== null) {
+    imageMovie.src = "https://image.tmdb.org/t/p/w1280" + newMovie.poster_path;
+    imageMovie.alt = `${newMovie.original_title} image`;
+  } else {
+    imageMovie.src = "assets/img/Noimage.svg.png";
+  }
+
+  const titleMovie = movieModalTemplate.querySelector(".info__title");
+  titleMovie.textContent = newMovie.title;
+
+  const ratingMovie = movieModalTemplate.querySelector(".info__rating");
+
+  ratingMovie.textContent = newMovie.vote_average;
+
+  if (newMovie.vote_average >= 7) {
+    ratingMovie.style.color = "#3BB33B";
+  } else if (newMovie.vote_average >= 5) {
+    ratingMovie.style.color = "#AAAAAA";
+  } else {
+    ratingMovie.style.color = "#FB0007";
+  }
+
+  const ratingCountMovie = movieModalTemplate.querySelector(
+    ".info__rating-count"
+  );
+  ratingCountMovie.textContent = "Rating count: " + newMovie.vote_count;
+
+  const popularityMovie = movieModalTemplate.querySelector(".info__popularity");
+  popularityMovie.textContent = "Popularity: " + newMovie.popularity;
+
+  const releaseMovie = movieModalTemplate.querySelector(".info__release");
+  releaseMovie.textContent = "Release: " + newMovie.release_date;
+
+  const languageMovie = movieModalTemplate.querySelector(".info__language");
+  languageMovie.textContent =
+    "Original language: " + newMovie.original_language;
+
+  const overviewMovie = movieModalTemplate.querySelector(".info__overview");
+  overviewMovie.textContent = newMovie.overview;
+  
+
+  movieList.appendChild(movieItem);
+
+  // mainmovie.addEventListener("click", function () {
+  //   // infoMovie.style.top = `${movieItem.offsetTop}px`;
+  //   // infoMovie.style.display = "flex";
+  // });
+
+  // infoMovie.addEventListener("click", function () {
+  //   infoMovie.style.display = "none";
+  // });
+}
 /*-------------------------------------------------------------*/
 /*-----Event listeners-----------------------------------------*/
 /*-------------------------------------------------------------*/
