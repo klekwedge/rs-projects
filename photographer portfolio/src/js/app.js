4e92e55db8ccd2e788ba7498a11e6911526b7e11
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-01 15:57:35 +0300

    fix: remove unused scripts

--- photographer portfolio/src/js/app.js
+++ photographer portfolio/src/js/app.js
@@ -3,7 +3,7 @@ import languageChange from "./modules/language.js";
 import burgerMenu from "./modules/burger.js";
 import themeChange from "./modules/theme.js";
 import pagination from "./modules/pagination.js";
-import isWebp from "./modules/functions.js";
+import isWebp from "./modules/webp.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   isWebp();
