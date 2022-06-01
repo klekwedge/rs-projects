1a63ef44b351cfc0f3b2627c205617c5cace777e
klekkwedge <antonov.andrey.10101@gmail.com>
2022-06-01 12:40:03 +0300

    refactor: change structure of the project

--- js30-3.2-memory-game/js/script.js
+++ js30-3.2-memory-game/js/script.js
@@ -1,24 +1,52 @@
-const cards = document.querySelectorAll(".memory-card");
-const score = document.querySelectorAll(".endgame__score");
-const moves = document.querySelector(".moves");
-const endGame = document.querySelector(".endgame");
-const resetGame = document.querySelector(".endgame__reset");
+const cards = document.querySelectorAll('.memory-card');
+const score = document.querySelectorAll('.endgame__score');
+const moves = document.querySelector('.moves');
+const endGame = document.querySelector('.endgame');
+const resetGame = document.querySelector('.endgame__reset');
 
 let hasFlippedCard = false;
 let lockBoard = false;
-let firstCard, secondCard;
+let firstCard;
+let secondCard;
 let moveCounter = 0;
 let progressCounter = 0;
 
-let arrBuff = Array(9);
-localStorage.setItem("array", JSON.stringify(arrBuff));
+const arrBuff = Array(9);
+localStorage.setItem('array', JSON.stringify(arrBuff));
 
 (function shuffle() {
   cards.forEach((card) => {
-    let randomPos = Math.floor(Math.random() * 20);
+    const randomPos = Math.floor(Math.random() * 20);
     card.style.order = randomPos;
   });
-})();
+}());
+
+function resetBoard() {
+  hasFlippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+  moves.textContent = `Your moves: ${moveCounter}`;
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetBoard();
+  }, 1000);
+}
+
+function checkForMatch() {
+  moveCounter += 1;
+  const isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  if (isMatch) {
+    disableCards();
+  } else {
+    unflipCards();
+  }
+}
 
 function flipCard() {
   if (lockBoard) {
@@ -29,7 +57,7 @@ function flipCard() {
     return;
   }
 
-  this.classList.add("flip");
+  this.classList.add('flip');
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
@@ -39,69 +67,46 @@ function flipCard() {
   }
 }
 
-function checkForMatch() {
-  moveCounter++;
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-  isMatch ? disableCards() : unflipCards();
-}
-
 function disableCards() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 
-  firstCard.style.background = "#86eb9e";
-  secondCard.style.background = "#86eb9e";
+  firstCard.style.background = '#86eb9e';
+  secondCard.style.background = '#86eb9e';
 
   resetBoard();
-  progressCounter++;
+  progressCounter += 1;
   if (progressCounter === 10) {
-    endGame.style.display = "block";
+    endGame.style.display = 'block';
     cards.forEach((card) => {
-      card.classList.remove("flip");
+      card.classList.remove('flip');
     });
 
     arrBuff.pop;
     arrBuff.unshift(moveCounter);
-    localStorage.setItem("array", JSON.stringify(arrBuff));
+    localStorage.setItem('array', JSON.stringify(arrBuff));
 
-    let arrScore = localStorage.getItem("array");
+    let arrScore = localStorage.getItem('array');
     arrScore = JSON.parse(arrScore);
-    for(let i = 0; i < arrScore.length; i++){
+    for (let i = 0; i < arrScore.length; i += 1) {
       score[i].textContent = arrScore[i];
     }
   }
 }
 
-function unflipCards() {
-  lockBoard = true;
-  setTimeout(() => {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
-    resetBoard();
-  }, 1000);
-}
-
-function resetBoard() {
-  hasFlippedCard = false;
-  lockBoard = false;
-  firstCard = null;
-  secondCard = null;
-  moves.textContent = `Your moves: ${moveCounter}`;
-}
-
-resetGame.addEventListener("click", function () {
-  endGame.style.display = "none";
+resetGame.addEventListener('click', () => {
+  endGame.style.display = 'none';
 
   moveCounter = 0;
   moves.textContent = `Your moves: ${moveCounter}`;
   progressCounter = 0;
 
   cards.forEach((card) => {
-    card.style.background = "linear-gradient(45deg, #fff1eb, #ace0f9)";
-    card.addEventListener("click", flipCard);
-    let randomPos = Math.floor(Math.random() * 20);
+    card.style.background = 'linear-gradient(45deg, #fff1eb, #ace0f9)';
+    card.addEventListener('click', flipCard);
+    const randomPos = Math.floor(Math.random() * 20);
     card.style.order = randomPos;
   });
 });
 
-cards.forEach((card) => card.addEventListener("click", flipCard));
+cards.forEach((card) => card.addEventListener('click', flipCard));
